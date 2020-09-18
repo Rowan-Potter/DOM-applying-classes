d3b246af754b1fb89b337e8808eb726c4ed71a02
Rowan Potter <Rowanfebruary@gmail.com>
2020-09-18 12:54:54 +1200

    Made element visible

--- classes.js
+++ classes.js
@@ -1,12 +1,12 @@
-// Don't change or delete this line! It waits until the DOM has loaded, then calls 
-// the start function. More info: 
+// Don't change or delete this line! It waits until the DOM has loaded, then calls
+// the start function. More info:
 // https://developer.mozilla.org/en-US/docs/Web/Events/DOMContentLoaded
 document.addEventListener('DOMContentLoaded', start)
 
 function start () {
   // The first example is done for you. Uncomment the line below and reload the browser.
-  // one()
-    
+   one()
+
   // Your turn! Create a new function called `two`, then call it from here.
 }
 
@@ -18,8 +18,32 @@ function one () {
   one.classList.add('blue')
 }
 
+
+
 // CREATE FUNCTION two HERE
+function start () {
+  two()
+}
 
+
+function two () {
+  var two = document.getElementById('two')
+  two.classList.add('green')
+}
 // CREATE FUNCTION three HERE
+function start () {
+  three()
+}
 
+function three () {
+  var three = document.getElementById('three')
+  three.classList.add('red')
+}
 // CREATE FUNCTION makeVisible HERE
+function start () {
+  makeVisible()
+}
+function makeVisible () {
+  var makeVisible = document.getElementsByClassName('invisible')[0]
+  makeVisible.classList.add('visible')
+}
